aacdd14ea7c23f5444af3e3c706333f393e65416
danangamw <danangamw@gmail.com>
2024-01-12 14:50:47 +0700

    chapter 4: parsing json Request

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -3,12 +3,46 @@ package main
 import (
 	"fmt"
 	"greenlight/internal/data"
+	"greenlight/internal/validator"
 	"net/http"
 	"time"
 )
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "create a new movie")
+	var input struct {
+		Title    string        `json:"title"`
+		Year     int32         `json:"year"`
+		Runtinme data.Runtinme `json:"runtime"`
+		Genres   []string      `json:"genres"`
+	}
+
+	// Use the new readJSON() helper to decode the request body into the input struct.
+	// If this returns an error we send the client the error message along with a 400
+	// Bad Request status code, just like before.
+	err := app.readJSON(w, r, &input)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	movie := &data.Movie{
+		Title:    input.Title,
+		Year:     input.Year,
+		Runtinme: input.Runtinme,
+		Genres:   input.Genres,
+	}
+
+	// Initialize a new validator instance.
+	v := validator.New()
+
+	// Call the ValidateMovie() function and return a response containing the erros if
+	// any of the check fail.
+	if data.ValidateMovie(v, movie); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+
+	fmt.Fprintf(w, "%+v\n", input)
 }
 
 func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
